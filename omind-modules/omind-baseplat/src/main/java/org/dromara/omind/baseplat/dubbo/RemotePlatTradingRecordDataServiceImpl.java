52bc021b019b2297435f775a29c112ff3cefb947
webVueBlog <2979986590@qq.com>
2024-09-08 20:58:59 +0800

    充电桩Saas云平台

---
+++ omind-modules/omind-baseplat/src/main/java/org/dromara/omind/baseplat/dubbo/RemotePlatTradingRecordDataServiceImpl.java
@@ -0,0 +1,31 @@
+package org.dromara.omind.baseplat.dubbo;
+
+import org.apache.dubbo.config.annotation.DubboService;
+import org.dromara.omind.baseplat.api.domain.entity.PlatTradingRecordData;
+import org.dromara.omind.baseplat.api.service.pile.RemotePlatTradingRecordDataService;
+import org.dromara.omind.baseplat.service.pile.PlatTradingRecordDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+/**
+ * 远程交易记录数据服务实现类
+ */
+@DubboService
+@Service
+public class RemotePlatTradingRecordDataServiceImpl implements RemotePlatTradingRecordDataService {
+
+    @Autowired
+    @Lazy
+    PlatTradingRecordDataService platTradingRecordDataService;
+
+    @Override
+    public PlatTradingRecordData getByTradeNo(String tradeNo) {
+        return platTradingRecordDataService.getByTradeNo(tradeNo);
+    }
+
+    @Override
+    public boolean save(PlatTradingRecordData platTradingRecordData) {
+        return platTradingRecordDataService.save(platTradingRecordData);
+    }
+}
---
+++ ruoyi-modules/ruoyi-resource/src/main/java/org/dromara/resource/mapper/SysOssConfigMapper.java
@@ -0,0 +1,17 @@
+package org.dromara.resource.mapper;
+
+
+import org.dromara.common.mybatis.core.mapper.BaseMapperPlus;
+import org.dromara.resource.domain.SysOssConfig;
+import org.dromara.resource.domain.vo.SysOssConfigVo;
+
+/**
+ * 对象存储配置Mapper接口
+ *
+ * 
+ * 
+ * @date 2021-08-13
+ */
+public interface SysOssConfigMapper extends BaseMapperPlus<SysOssConfig, SysOssConfigVo> {
+
+}
